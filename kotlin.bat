20ea43e1959347ca04e1a57aeb56ed31ecbb5379
Suresh G <sureshg.g@gmail.com>
2026-08-08 18:15:33 -0700

    ci: bump Kotlin toolchain and update config

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4220
+set kotlin_cli_version=0.12.0-dev-4232
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=c6c6022714abe6cddc4660c43e4dcb2217c48e27c5cb297541d1eaacd5087256
+set kotlin_cli_sha256=4bc5e2c39e696e76e104be942b5f5821c8f9adc878c9abae2feb790065872aab
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
